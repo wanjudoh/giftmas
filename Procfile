e715df70ac90569150877430ed5f0fa2618be702
ehdhkswn1997 <ehdhkswn@naver.com>
2019-12-23 01:56:02 +0900

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn letterProject.wsgi --log-file -
--- letterProject/settings.py
+++ letterProject/settings.py
@@ -125,3 +125,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'letter', 'static')
 ]
+
+# Heroku: Update database configuration 
+from $DATABASE_URL. import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
