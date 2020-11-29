5b5ce7c08f86aa795d4cd289ff35b3e97bd1f9c0
wanjudoh <ehdhkswn@naver.com>
2020-11-30 01:05:23 +0900

    링크 수정~

--- letter/urls.py
+++ letter/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('new', views.new, name='new'),
     path('detail/<int:letter_id>', views.detail, name='detail'),
     path('gift', views.gift, name='gift'),
+    path('giftsave', views.giftsave, name='giftsave'),
+
 ]
--- letter/views.py
+++ letter/views.py
@@ -12,7 +12,7 @@ def home(request):
 def new(request):
     if request.method == 'POST':
         form = LetterForm(request.POST)
-        gift = request.POST['gift']
+        # gift = request.POST['gift']
         if form.is_valid():
             #if len(form.cleaned_data['pw']) < 4:
             #    return render(request, 'new.html', {'form':form, 'error':'비밀번호는 4자로 입력합니다.', 'gift':gift})
@@ -20,20 +20,35 @@ def new(request):
             letter.title = form.cleaned_data['title']
             letter.content = form.cleaned_data['content']
             letter.name = form.cleaned_data['name']
-            letter.gift = gift
+            letter.gift = 0
             letter.design = request.POST['design']
             #letter.sender = socket.gethostbyname(socket.gethostname()) #보내는사람 IP주소 저장
             letter.save()
-            return render(request, 'link.html', {'letter':letter})
+            # return render(request, 'link.html', {'letter':letter})
+            return render(request, 'gift.html', {'pk':letter.pk})
         else:
-            return HttpResponse('폼이 유효하지 않다는데요..? 다시입력해보세요 ㅎ')
+            return HttpResponse('알 수 없는 에러가 발생했습니다 ㅠㅠ 다시입력해주세요!')
     else:
         form = LetterForm()
-        gift = request.GET['gift']
-        return render(request, 'new.html', {'form':form, 'gift':gift})
+        # gift = request.GET['gift']
+        # return render(request, 'new.html', {'form':form, 'gift':gift})
+        return render(request, 'new.html', {'form':form})
 
 def gift(request):
     return render(request, 'gift.html')
+
+def giftsave(request):
+    if request.method == 'POST':
+        gift = request.POST['gift']
+        pk = request.POST['pk']
+        letter = Letter.objects.get(pk=pk)
+        if gift == '0':
+            import random
+            letter.gift = random.randint(1, 6)
+        else:
+            letter.gift = gift
+        letter.save()
+        return render(request, 'link.html', {'letter':letter})
 
 def detail(request, letter_id):
     letter = get_object_or_404(Letter, pk=letter_id)
